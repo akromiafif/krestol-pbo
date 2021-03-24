8514b1670f6405c578b3c0a4169a415125baf2ed
rdx199 <64213179+rdx199@users.noreply.github.com>
2021-03-24 22:42:14 +0700

    add Player.h

---
+++ inc/Player.h
@@ -0,0 +1,32 @@
+// Player.h
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+#include "tubes1-oop/inc/Engimon.h"
+#include "tubes1-oop/inc/skill/Skill.h"
+#include "Inventory.h"
+
+class Player{
+    private:
+        void MoveActiveEngimon(); // engimon active
+        int inputCommand(); // command input player
+    protected:
+        // inventory
+        Inventory<Engimon, Skill> Inven;
+    public:
+        Player();
+        ~Player();
+        void setActiveEngimon(Engimon* Engi); // set engimon active
+        void MovePlayer(); // bergerak di map ?
+        void showEngimonList(); // menampilkan list engimon
+        void showEngimonDetail(); // menampilkan data lengkap engimon
+        void showSkillList(); // menampilkan list skill item
+        void useSkilltoEngimon(); // menggunakan skill item ke Engimon
+        void interacttoEngimon(); // interaksi dengan engimon active
+        void beginBreeding(Engimon* Engi1, Engimon* Engi2); // melaksanakan breeding
+        void beginBattle(); // melakukan battle
+};
+
+#endif
